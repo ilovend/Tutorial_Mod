75807852693b3aaecf93bdc9664b7a3046133c23
ilovend <ilovend@qq.com>
2024-07-20 00:29:41 +0800

    Add two new Item

--- src/main/java/cc/ilovend/TutorialMod.java
+++ src/main/java/cc/ilovend/TutorialMod.java
@@ -1,5 +1,7 @@
 package cc.ilovend;
 
+import cc.ilovend.registry.ModItemGroup;
+import cc.ilovend.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,6 +11,8 @@ public class TutorialMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final String MOD_ID = "tutorial-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger("tutorial-mod");
 
 	@Override
@@ -18,5 +22,8 @@ public class TutorialMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello world!");
+
+		ModItemGroup.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
